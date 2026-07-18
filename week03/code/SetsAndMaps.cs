5a126a8b15c6de889b2f57eb563371bf2125488a
medicenhoy <emartinezcabello@byupathway.edu>
2026-07-18 20:33:10 -0300

    005

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,22 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var seen = new HashSet<string>();
+        var result = new List<string>();
+
+            foreach (var word in words)
+            {
+                var reverse = $"{word[1]}{word[0]}";
+
+                if (word != reverse && seen.Contains(reverse))
+                {
+                    result.Add($"{word} & {reverse}");
+                }
+
+                seen.Add(word);
+            }
+
+        return result.ToArray();
     }
 
     /// <summary>
@@ -39,10 +53,17 @@ public static class SetsAndMaps
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
+
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+
+            var degree = fields[3];
+
+            if (degrees.ContainsKey(degree))
+                degrees[degree]++;
+            else
+                degrees[degree] = 1;
         }
 
         return degrees;
@@ -66,8 +87,34 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        if (word1.Length != word2.Length)
+            return false;
+
+        var counts = new Dictionary<char, int>();
+
+        foreach (char letter in word1)
+        {
+            if (counts.ContainsKey(letter))
+                counts[letter]++;
+            else
+                counts[letter] = 1;
+        }
+
+        foreach (char letter in word2)
+        {
+            if (!counts.ContainsKey(letter))
+                return false;
+
+            counts[letter]--;
+
+            if (counts[letter] < 0)
+                return false;
+        }
+
+        return true;
     }
 
     /// <summary>
@@ -87,20 +134,33 @@ public static class SetsAndMaps
     public static string[] EarthquakeDailySummary()
     {
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
         using var client = new HttpClient();
         using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
         using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
         using var reader = new StreamReader(jsonStream);
+
         var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        var featureCollection =
+            JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+        if (featureCollection == null)
+            return [];
+        var results = new List<string>();
+
+        foreach (var feature in featureCollection.Features)
+        {
+            results.Add(
+                $"{feature.Properties.Place} - Mag {feature.Properties.Mag}"
+            );
+        }
+
+        return results.ToArray();
     }
 }
